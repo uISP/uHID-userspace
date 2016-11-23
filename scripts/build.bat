804b089d98950eff9d80a925c619867b8b96c1a2
Andrew Andrianov <andrew@ncrmnt.org>
2016-11-23 23:51:14 +0300

    scripts: Pass arguments to cmake
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,6 +1,6 @@
 rmdir /S /Q build
 mkdir build
 cd build
-cmake .. -G "MinGW Makefiles" -DUHID_HIDAPI_DIR=C:\libs\hidapi -DCMAKE_BUILD_TYPE=StaticRelease
+cmake .. -G "MinGW Makefiles" -DUHID_HIDAPI_DIR=C:\libs\hidapi -DCMAKE_BUILD_TYPE=StaticRelease %*
 mingw32-make
 mingw32-make package
